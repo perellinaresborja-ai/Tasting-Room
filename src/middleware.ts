98fc4779412cf69289862b42f0bb76e3212a3b72
Borja Perez <perellinaresborja@gmail.com>
2026-09-09 20:30:36 +0200

    Fix middleware matcher to catch and redirect non-locale routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,5 +47,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/(es|en)/:path*']
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
